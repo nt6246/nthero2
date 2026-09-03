a92db03b0046a3905c633974d05edecd2621af4d
nt6246 <nt6246@gmail.com>
2026-09-04 05:15:35 +0700

    Updated receiver.js

--- bolt/receiver.js
+++ bolt/receiver.js
@@ -2,7 +2,11 @@ const { ExpressReceiver } = require('@slack/bolt')
 
 const receiver = (expressApp) => {
   return new ExpressReceiver({
-    app: expressApp
+    app: expressApp,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    endpoints: {
+      events: '/slack/events',
+    },
   });
 };
 
