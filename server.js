137e1baeacbb68e4eba4a6c513906a61fb0bc2fd
nt6246 <nt6246@gmail.com>
2026-09-04 07:07:46 +0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 'use strict';
-
-const path = require('path');
-
 const app = require('express')();
-
-const bolt = require('./bolt');
-require('dotenv').config();
 
 // const authenticate = require('./src/authenticate');
 const params = require('./src/params');
